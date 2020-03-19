a769a8db2cb7ef899dc5143c1af15846552f8dd7
Igor Anufriev <sacmi@pm.me>
2020-03-19 23:41:54 +0700

    я чтота сделяль

---
+++ precache-manifest.ecaae6afc8b61867df12e66bb4eb04a9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b61dbe4914cc2f2162",
+    "url": "/physics-client/css/base.04e693f2.css"
+  },
+  {
+    "revision": "102b3e30a48497e05e32",
+    "url": "/physics-client/css/chunk-59bfea8e.55e28d86.css"
+  },
+  {
+    "revision": "3475d8df15b0f2e0f703",
+    "url": "/physics-client/css/chunk-775fb1e4.bcd91a18.css"
+  },
+  {
+    "revision": "27f7e2a9bdad16246d62",
+    "url": "/physics-client/css/chunk-d49f221e.207437e9.css"
+  },
+  {
+    "revision": "c8d37878c3c085e6d1d4",
+    "url": "/physics-client/css/chunk-e3c1b3ee.8c0bc423.css"
+  },
+  {
+    "revision": "03c4051be595e98d27da",
+    "url": "/physics-client/css/chunk-vendors.01f4d406.css"
+  },
+  {
+    "revision": "0b61e2020771f3112a3a0bb97278ca4d",
+    "url": "/physics-client/index.html"
+  },
+  {
+    "revision": "68380e0d3fc2551eb97b",
+    "url": "/physics-client/js/app.642b2c0e.js"
+  },
+  {
+    "revision": "79b61dbe4914cc2f2162",
+    "url": "/physics-client/js/base.f34aff51.js"
+  },
+  {
+    "revision": "102b3e30a48497e05e32",
+    "url": "/physics-client/js/chunk-59bfea8e.431d96fb.js"
+  },
+  {
+    "revision": "3475d8df15b0f2e0f703",
+    "url": "/physics-client/js/chunk-775fb1e4.a428463a.js"
+  },
+  {
+    "revision": "27f7e2a9bdad16246d62",
+    "url": "/physics-client/js/chunk-d49f221e.44f7a122.js"
+  },
+  {
+    "revision": "c8d37878c3c085e6d1d4",
+    "url": "/physics-client/js/chunk-e3c1b3ee.19cbcc0c.js"
+  },
+  {
+    "revision": "03c4051be595e98d27da",
+    "url": "/physics-client/js/chunk-vendors.24bf2f91.js"
+  },
+  {
+    "revision": "f3c489ee00b0678e7369db1341cf2a13",
+    "url": "/physics-client/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/physics-client/robots.txt"
+  }
+]);
