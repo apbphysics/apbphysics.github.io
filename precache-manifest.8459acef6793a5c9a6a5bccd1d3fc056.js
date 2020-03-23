3cb193d9c2be8faa3b3bcb72e0c3b99e4c878aa3
Igor Anufriev <sacmi@pm.me>
2020-03-24 00:00:40 +0700

    Some improvements

---
+++ precache-manifest.8459acef6793a5c9a6a5bccd1d3fc056.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd55d1232f856f70efd",
+    "url": "https://apbphysics.github.io/css/chunk-3f6904dd.5daba9ed.css"
+  },
+  {
+    "revision": "a0dc36bf1795f5d3d72d",
+    "url": "https://apbphysics.github.io/css/chunk-509e66f8.a7e03b85.css"
+  },
+  {
+    "revision": "b0235baf184492cbeef7",
+    "url": "https://apbphysics.github.io/css/chunk-86c7129a.d46ff973.css"
+  },
+  {
+    "revision": "393a9b861f3becdcdffb",
+    "url": "https://apbphysics.github.io/css/chunk-vendors.bfcd004a.css"
+  },
+  {
+    "revision": "087b7f5d776fb765600564c8fd079b8a",
+    "url": "https://apbphysics.github.io/index.html"
+  },
+  {
+    "revision": "4a3cf97613fecd19de79",
+    "url": "https://apbphysics.github.io/js/app-legacy.8c87c334.js"
+  },
+  {
+    "revision": "78af3cc9c811f11eef51",
+    "url": "https://apbphysics.github.io/js/chunk-2d21a3d2-legacy.5398ede4.js"
+  },
+  {
+    "revision": "4fd55d1232f856f70efd",
+    "url": "https://apbphysics.github.io/js/chunk-3f6904dd-legacy.3cd38632.js"
+  },
+  {
+    "revision": "a0dc36bf1795f5d3d72d",
+    "url": "https://apbphysics.github.io/js/chunk-509e66f8-legacy.74e7a877.js"
+  },
+  {
+    "revision": "b0235baf184492cbeef7",
+    "url": "https://apbphysics.github.io/js/chunk-86c7129a-legacy.e78bbe76.js"
+  },
+  {
+    "revision": "393a9b861f3becdcdffb",
+    "url": "https://apbphysics.github.io/js/chunk-vendors-legacy.db546e09.js"
+  },
+  {
+    "revision": "f3c489ee00b0678e7369db1341cf2a13",
+    "url": "https://apbphysics.github.io/manifest.json"
+  }
+]);
